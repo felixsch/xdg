c72eed2758133a3b4a6ce2f4c9bbbf02d66fa757
Felix Schnizlein <felix@fix3d.net>
2014-03-30 23:56:52 +0200

    Added exec functionality

--- src/System/Environment/XDG/DesktopEntry.hs
+++ src/System/Environment/XDG/DesktopEntry.hs
@@ -5,46 +5,47 @@ module System.Environment.XDG.DesktopEntry
     ( EntryType(..)
     , DesktopEntry
     , loadEntry
+    , saveEntry
     , getName
     , getType
     , getEntryValue
+    , getExec
+    , execEntry
     , getLocalizedEntryValue
     , typeFromText
-    , getC
---    , deLoadFile
---    , deSaveFile )
-    )
+    , getC )
     where
 
-import Prelude hiding (unlines)
+import Prelude
 import Control.Applicative
 import Data.Maybe
-import Data.Text hiding (map, concatMap, concat)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Map as M
+import System.Process
 
 import System.Environment.XDG.Internal.Ini
 
 type DesktopEntry = IniFile
-type LocaleString = M.Map Text Text
+type LocaleString = M.Map T.Text T.Text
 
-data EntryType = Application | Directory | Link | Unknown Text
+data EntryType = Application | Directory | Link | Unknown T.Text
     deriving (Show, Eq)
 
-typeFromText :: Text -> EntryType
+typeFromText :: T.Text -> EntryType
 typeFromText "Application" = Application
 typeFromText "Directory"   = Directory
 typeFromText "Link"        = Link
 typeFromText x             = Unknown x
 
 
-getEntryValue :: (FromValue a) => Text -> DesktopEntry -> Maybe a
+getEntryValue :: (FromValue a) => T.Text -> DesktopEntry -> Maybe a
 getEntryValue = getValue "Desktop Entry"
 
-getLocalizedEntryValue :: (FromValue a) => Text -> DesktopEntry -> Maybe (M.Map Text a)
+getLocalizedEntryValue :: (FromValue a) => T.Text -> DesktopEntry -> Maybe (M.Map T.Text a)
 getLocalizedEntryValue = getValueAll "Desktop Entry"
 
-getC :: LocaleString -> Text
+getC :: LocaleString -> T.Text
 getC = fromJust . M.lookup "C" 
 
 
@@ -55,7 +56,7 @@ loadEntry path = check =<< decodeIni <$> TIO.readFile path
         check (Left _) = error "Could not load desktop entry file"
 
 saveEntry :: FilePath -> DesktopEntry -> IO ()
-saveEntry path entry = TIO.writeFile path (unlines $ encodeIni entry)
+saveEntry path entry = TIO.writeFile path (T.unlines $ encodeIni entry)
 
 getType :: DesktopEntry -> EntryType
 getType = typeFromText . fromJust . getEntryValue "Type"
@@ -63,21 +64,58 @@ getType = typeFromText . fromJust . getEntryValue "Type"
 getName :: DesktopEntry -> LocaleString
 getName = fromJust . getLocalizedEntryValue "Name"
 
+getIcon :: DesktopEntry -> Maybe T.Text
+getIcon = getEntryValue "Icon"
+
 getGenericName :: DesktopEntry -> Maybe LocaleString
 getGenericName = getLocalizedEntryValue "GenericName"
 
-getPath :: DesktopEntry -> Maybe Text
+getPath :: DesktopEntry -> Maybe T.Text
 getPath = getEntryValue "Path"
 
 getTerminal :: DesktopEntry -> Maybe Bool
 getTerminal = getEntryValue "Terminal"
 
-getExec :: DesktopEntry -> Maybe Text
+getExec :: DesktopEntry -> Maybe T.Text
 getExec = getEntryValue "Exec"
 
-
-getVersion :: DesktopEntry -> Maybe Text
+getVersion :: DesktopEntry -> Maybe T.Text
 getVersion = getEntryValue "Version"
+
+
+execEntry :: DesktopEntry -> IO ProcessHandle
+execEntry entry = spawnCommand =<< T.unpack <$> cmd exec
+    where
+        cmd (x:_) = return x
+        cmd []     = error "Invalid command specified as 'Exec' value"
+        exec = maybe [] T.words $ getExec entry
+
+execEntryWith :: [T.Text] -> [T.Text] -> DesktopEntry -> IO ProcessHandle
+execEntryWith files urls entry = do
+    c <- cmd exec
+    a <- cargs exec
+    spawnProcess c a
+    where
+        cmd (x:xs) = return $ T.unpack x
+        cmd []    = error "Invalid command specified as 'Exec' value"
+
+        cargs (_:xs) = return $ map T.unpack $ map re xs
+        exec = maybe [] T.words $ getExec entry
+
+        first [] = T.empty
+        first x  = head x
+
+        re "%f" = first files
+        re "%F" = T.unwords files
+        re "%u" = first urls
+        re "%U" = T.unwords urls
+        re x    = x
+
+
+
+
+
+
 
 
 
