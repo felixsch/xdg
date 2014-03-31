568c5ea5945ddb7e00e97380b8d4b4a97e085462
Felix Schnizlein <felix@fix3d.net>
2014-04-01 01:03:50 +0200

    execEntryWith added

--- src/System/Environment/XDG/DesktopEntry.hs
+++ src/System/Environment/XDG/DesktopEntry.hs
@@ -11,6 +11,7 @@ module System.Environment.XDG.DesktopEntry
     , getEntryValue
     , getExec
     , execEntry
+    , execEntryWith
     , getLocalizedEntryValue
     , typeFromText
     , getC )
@@ -109,12 +110,8 @@ execEntryWith files urls entry = do
         re "%F" = T.unwords files
         re "%u" = first urls
         re "%U" = T.unwords urls
+        re "%i" = maybe T.empty (\x -> T.unwords ["--icon", x]) $ getIcon entry
         re x    = x
-
-
-
-
-
 
 
 
@@ -143,87 +140,4 @@ data DesktopEntry = DesktopEntry
   , deStartupNotify :: Maybe Bool
   , deStartupWMClass :: Maybe Text
   , deURL :: Maybe Text }
-  deriving (Show)
-
-
-newDesktopEntry :: EntryType -> Text -> DesktopEntry
-newDesktopEntry type' name = DesktopEntry
-    { deType = type'
-    , deName = M.fromList [("C", name)]
-    , deVersion = Nothing
-    , deGenericName = Nothing
-    , deNoDisplay = Nothing
-    , deComment = Nothing
-    , deIcon = Nothing
-    , deHidden = Nothing
-    , deOnlyShowIn = Nothing
-    , deNotShowIn = Nothing
-    , deDBusActivatable = Nothing
-    , deTryExec = Nothing
-    , deExec = Nothing
-    , dePath = Nothing
-    , deTerminal = Nothing
-    , deActions = Nothing
-    , deMimeType = Nothing
-    , deCategories = Nothing
-    , deKeywords = Nothing
-    , deStartupNotify = Nothing
-    , deStartupWMClass = Nothing
-    , deURL = Nothing
-    }
-
-iniToDesktopEntry :: IniFile -> IO DesktopEntry
-iniToDesktopEntry i = return $ DesktopEntry
-    { deType = typeFromText $ strict "Type" $ get "Type"
-    , deVersion = get "Version"
-    , deName = strict "Name" $ getAll "Name"
-    , deGenericName = getAll "GenericName"
-    , deNoDisplay = get "NoDisplay"
-    , deComment = getAll "Comment"
-    , deIcon = getAll "Icon"
-    , deHidden = get "Hidden"
-    , deOnlyShowIn = get "OnlyShowIn"
-    , deNotShowIn = get "NotShowIn"
-    , deDBusActivatable = get "DBusActivatable"
-    , deTryExec = get "TryExec"
-    , deExec = get "Exec"
-    , dePath = get "Path"
-    , deTerminal = get "Terminal"
-    , deActions = get "Actions"
-    , deMimeType = get "MimeType"
-    , deCategories = get "Categories"
-    , deKeywords = getAll "Keywords"
-    , deStartupNotify = get "StartupNotify"
-    , deStartupWMClass = get "StartupWMClass"
-    , deURL = get "URL" }
-    where
-      strict _ (Just x) = x
-      strict k Nothing  = error $ "Could not find mandatory key: " ++ k 
-
-      get x = getValue "Desktop Entry" x i
-      getAll x = getValueAll "Desktop Entry" x i
-
-
-
-
-
-
-fromMaybeWith ::  (Text -> a -> [IniPair]) -> Text -> Maybe a -> [IniPair]
-fromMaybeWith _ _ Nothing = []
-fromMaybeWith f k (Just v) = f k v
-
-
-
-mapToList :: (ToValue a) => Text -> M.Map Text a -> [IniPair]
-mapToList k = M.foldrWithKey (\i x xs -> (k, (Just i, toValue x)):xs) []
-
-
-
-
-deLoadFile :: FilePath -> IO DesktopEntry
-deLoadFile path = iniToDesktopEntry  =<< decodeIni =<< TIO.readFile path
-
-deSaveFile :: FilePath -> DesktopEntry -> IO ()
-deSaveFile path d = TIO.writeFile path =<< unlines . encodeIni <$> desktopToIniFile d
-
--}
+  deriving (Show) -}
