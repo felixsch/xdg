9cf24e554c3f4198dc9487a42161e795747d4166
Felix Schnizlein <felix@fix3d.net>
2014-05-29 00:55:58 +0200

    Started with the menu implementation

--- src/System/Environment/XDG/Menu.hs
+++ src/System/Environment/XDG/Menu.hs
@@ -1,9 +1,12 @@
 module System.Environment.XDG.Menu
-  (
+  ( getUserMenuDir, getUserMenuDirWith
+  , getMenuDirs, getMenuDirsWith
   ) where
 
 
 import Control.Applicative
+
+import System.FilePath ((</>))
 
 import qualified Text.XML.Light as XML
 
@@ -12,4 +15,17 @@ import System.Environment.XDG.BaseDir
 
 getUserMenuDir :: IO FilePath
 getUserMenuDir
-    = getUserMenuDir
+    = (</> "menus" </> "application.menu") <$> getUserConfigDir
+
+getUserMenuDirWith :: String -> IO FilePath
+getUserMenuDirWith
+    de = (</> "menus" </> de ++ "-application.menu") <$> getUserConfigDir
+
+getMenuDirs :: IO [FilePath]
+getMenuDirs
+    = map (</> "menu" </> "application.menu") <$> getConfigDirs
+
+
+getMenuDirsWith :: String -> IO [FilePath]
+getMenuDirsWith
+    de = map (</> "menus" </> de ++ "-application.menu") <$> getConfigDirs
